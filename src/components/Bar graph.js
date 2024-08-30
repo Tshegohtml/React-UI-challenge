16462f959c690e425d6a2cf5399894d0b53b41da
Andilemlandu93 <mlanduandile5@gmail.com>
2024-08-30 14:40:32 +0200

    fifth

--- src/components/Bar graph.js
+++ src/components/Bar graph.js
@@ -1,49 +1,56 @@
 import React from "react";
 
-
 function BarGraph() {
   return (
     <div className="main-container">
       <div className="App">
         <h1>Spending - Last 7 Days</h1>
         <div className="graph-container">
-          <div className="graph-bar-container graph-bar-container-monday">
-            <div className="bar"></div>
-            <h1>Mon</h1>
+          <div className="box-container">
+            <div className="bar-container">
+              <div className="graph-bar-container" id="graph-bar-container-monday"></div>
+              <small>mon</small>
+            </div>
+            <div className="bar-container">
+              <div className="graph-bar-container" id="graph-bar-container-tuesday"></div>
+              <small>tues</small>
+            </div>
+            <div className="bar-container">
+              <div className="graph-bar-container" id="graph-bar-container-wednesday"></div>
+              <small>wed</small>
+            </div>
+            <div className="bar-container">
+              <div className="graph-bar-container" id="graph-bar-container-thursday"></div>
+              <small>thurs</small>
+            </div>
+            <div className="bar-container">
+              <div className="graph-bar-container" id="graph-bar-container-friday">
+                <div className="bar"></div>
+              </div>
+              <small>fri</small>
+            </div>
+            <div className="bar-container">
+              <div className="graph-bar-container" id="graph-bar-container-saturday">
+                <div className="bar"></div>
+              </div>
+              <small>sat</small>
+            </div>
+            <div className="bar-container">
+              <div className="graph-bar-container" id="graph-bar-container-sunday">
+                <div className="bar"></div>
+              </div>
+              <small>sun</small>
+            </div>
           </div>
-          <div className="graph-bar-container graph-bar-container-tuesday">
-            <div className="bar"></div>
-            <h1>Tue</h1>
-          </div>
-          <div className="graph-bar-container graph-bar-container-wednesday">
-            <div className="bar"></div>
-            <h1>Wed</h1>
-          </div>
-          <div className="graph-bar-container graph-bar-container-thursday">
-            <div className="bar"></div>
-            <h1>Thu</h1>
-          </div>
-          <div className="graph-bar-container graph-bar-container-friday">
-            <div className="bar"></div>
-            <h1>Fri</h1>
-          </div>
-          <div className="graph-bar-container graph-bar-container-saturday">
-            <div className="bar"></div>
-            <h1>Sat</h1>
-          </div>
-          <div className="graph-bar-container graph-bar-container-sunday">
-            <div className="bar"></div>
-            <h1>Sun</h1>
-          </div>
-        </div>
-        <div className="paragraph">
-          <div>
-            <h1>Total this month</h1>
-            <h2>$478.33</h2>
-          </div>
-          <div>
-            <h1>+2.4%</h1>
-            <h2>from last month</h2>
+          <div className="paragraph-container">
+            <div className="paragraph-left">
+              <small>Total this month</small>
+              <h2>$478.33</h2>
+            </div>
+            <div className="paragraph-right">
+              <h1>+2.4%</h1>
+              <small>from last month</small>
+            </div>
           </div>
         </div>
       </div>
@@ -52,3 +59,4 @@ function BarGraph() {
 }
 
 export default BarGraph;
+
